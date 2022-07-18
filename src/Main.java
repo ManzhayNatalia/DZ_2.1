ad1e0dd117f829a84d17461d0fa9d65e802f4045
Natalia <2240344@gmail.com>
2022-07-18 20:08:49 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 3000;
+        int mile = price / 20;
+        System.out.println("Количество начисленных миль " + mile);
+
+    }
+}
