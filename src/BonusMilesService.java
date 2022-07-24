e5f57cb47cf4f041cbb2bffc5d2aee3a0dc628ea
Natalia <2240344@gmail.com>
2022-07-24 16:14:03 -0300

    init commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+
+        int mile = cost / 20;
+        return mile;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        int price = 3000;
-        int mile = price / 20;
-        System.out.println("Количество начисленных миль " + mile);
 
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
